01e3643dbf4927e14151552ef67e8600c0c6e76b
User <User@DESKTOP-FJ7FH92>
2024-11-12 01:20:02 +0200

    Add project files.

---
+++ Controllers/FruitsController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab9.Controllers
+{
+    public class FruitsController : Controller
+    {
+        public IActionResult FirstViewMethod()
+        {
+            return View(GetListOfFruits());
+        }
+
+        public IActionResult SecondViewMethod()
+        {
+            return View(GetListOfFruits().OrderBy(x => x).ToList());
+        }
+        public IActionResult ThirdViewMethod()
+        {
+            return View(GetListOfFruits().OrderBy(x => x).ToList());
+        }
+
+
+        private List<string> GetListOfFruits()
+        {
+            return new List<string> {
+                "Apple",
+                "Banana",
+                "Orange",
+                "Lemon",
+                "Grape",
+                "Plum",
+                "Melon"
+            };
+        }
+    }
+}
